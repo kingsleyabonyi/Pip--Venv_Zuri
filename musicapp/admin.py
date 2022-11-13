3d08ad7ce3d94113bf8351132f53637a4ab35d5e
kingsleyabonyi <kinabonyi@gmail.com>
2022-11-13 03:48:42 +0100

    created super user and added song CRUD operation with RestAPI

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Artiste
+from .models import Song
+from .models import Lyric
 
 # Register your models here.
+admin.site.register(Artiste)
+admin.site.register(Song)
+admin.site.register(Lyric)
--- musicapp/models.py
+++ musicapp/models.py
@@ -6,15 +6,24 @@ class  Artiste(models.Model):
     last_name = models.CharField(max_length=250)
     age = models.IntegerField()
 
+    def __str__ (self):
+        return self.first_name + '-' + self.last_name
+
 class  Song(models.Model):
     # artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     date_released = models.DateField(max_length=100)
-    likes = models.CharField(max_length=700)
-    artiste_id = models.CharField(max_length=250)
+    likes = models.IntegerField()
+    artiste_id = models.IntegerField()
+
+    def __str__ (self):
+        return self.title
 
 
 class Lyric(models.Model):
     # song = models.ForeignKey(Song, on_delete=models.CASCADE)
     content = models.CharField(max_length=500)
-    song_id = models.CharField(max_length=300)
+    song_id = models.IntegerField()
+
+    def __str__ (self):
+        return self.content
